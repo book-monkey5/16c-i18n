699989c6da88650a30169022ca17154dd8d9d9c4
The Buildbot <buildbot@angular2buch.de>
2022-08-30 23:23:35 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/466d7b4d312aa294b1a94809dc3d2500f59ac0fc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,4 @@ import { AuthService } from './shared/auth.service';
 })
 export class AppComponent {
   constructor(public auth: AuthService) {}
-
-  changeLocale(targetLang: string) {
-    localStorage.setItem('locale', targetLang);
-    location.reload();
-  }
 }
