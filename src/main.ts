b88fd6b6d9a62357a7a488b5cafd339a51ffd177
The Buildbot <buildbot@angular2buch.de>
2022-10-06 13:49:31 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/6562d2ae6aa5454d47fe63794a983ee982103920

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { enableProdMode } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { enableProdMode, LOCALE_ID } from '@angular/core';
+import { loadTranslations } from '@angular/localize';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import localeDe from '@angular/common/locales/de';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +11,21 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+async function setupLocale() {
+  if (localStorage.getItem('locale') !== 'de') {
+    return 'en-US';
+  }
+  const response = await fetch('assets/messages.de.json');
+  const result = await response.json();
+  loadTranslations(result.translations);
+  registerLocaleData(localeDe);
+  return 'de';
+}
+
+setupLocale().then(localeValue => {
+  platformBrowserDynamic([
+    { provide: LOCALE_ID, useValue: localeValue }
+  ])
+    .bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+});
